6ca06d1f2076d81fa5d03636c118cdfe185e6876
Jan Maaß <git@antipost.de>
2023-11-02 11:19:49 +0100

    Add solution

--- js/index.js
+++ js/index.js
@@ -4,10 +4,18 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
-addButton.addEventListener("click", () => {
-  // Exercise: Append a new entry to the toast messages container
+addButton.addEventListener("click", (event) => {
+  event.preventDefault();
+
+  const newMessage = document.createElement("li");
+  newMessage.classList.add("toast-container__message");
+
+  newMessage.textContent = "Another toast message.";
+
+  toastContainer.append(newMessage);
+
 });
 
 clearButton.addEventListener("click", () => {
-  // Exercise: Clear the stack of toast messages
+  toastContainer.innerHTML = "";
 });
