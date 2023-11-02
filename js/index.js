18dc14441d592aeec71d16488a6764d34ac6d283
Jan Maaß <git@antipost.de>
2023-11-02 11:38:41 +0100

    Remove preventDefault

--- js/index.js
+++ js/index.js
@@ -4,8 +4,7 @@ const toastContainer = document.querySelector('[data-js="toast-container"]');
 const addButton = document.querySelector('[data-js="add-button"]');
 const clearButton = document.querySelector('[data-js="clear-button"]');
 
-addButton.addEventListener("click", (event) => {
-  event.preventDefault();
+addButton.addEventListener("click", () => {
 
   const newMessage = document.createElement("li");
   newMessage.classList.add("toast-container__message");
